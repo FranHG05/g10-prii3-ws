8d50aca17c130fcede7e5826ba6970d9a2275b39
MarcosOlcinaUPV <molccom@epsa.upv.es>
2025-10-03 13:57:05 +0200

    Primera versión sin launch ni pausas

--- sprint1/v1/src/g10_prii3_turtlesim/setup.py
+++ sprint1/v1/src/g10_prii3_turtlesim/setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='marolc',
-    maintainer_email='marolc@todo.todo',
+    maintainer_email='molccom@epsa.upv.es',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
