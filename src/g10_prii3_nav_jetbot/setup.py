871bf40afccfb0674458fb022c7906ffa7238c01
MarcosOlcinaUPV <molccom@epsa.upv.es>
2025-11-11 13:23:10 +0100

    Implementación de nav_turtlebot

---
+++ src/g10_prii3_nav_jetbot/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'g10_prii3_nav_jetbot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='marolc',
+    maintainer_email='molccom@epsa.upv.es',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
---
+++ src/g10_prii3_nav_turtlebot/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'g10_prii3_nav_turtlebot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='marolc',
+    maintainer_email='molccom@epsa.upv.es',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'multipose = g10_prii3_nav_turtlebot.multipose:main',
+        ],
+    },
+)
